16deb87b22ac1cd00aecf6ae8ef903b32b1e6dde
Ubuntu <bqbrady@gmail.com>
2022-09-20 20:24:04 +0000

    Add validator endpoint to get leader schedule

--- beacon_client/validator_endpoints.py
+++ beacon_client/validator_endpoints.py
@@ -1,2 +1,14 @@
 class ValidatorEndpoints:
-    pass
+    def get_block_proposers_duties(self, epoch: int) -> dict:
+        """
+        Request beacon node to provide all validators that are scheduled to propose a block in the given epoch.
+        Duties should only need to be checked once per epoch, however a chain reorganization could occur that results in a change of duties.
+        For full safety, you should monitor head events and confirm the dependent root in this response matches:
+
+        event.current_duty_dependent_root when compute_epoch_at_slot(event.slot) == epoch
+        event.block otherwise
+        The dependent_root value is get_block_root_at_slot(state, compute_start_slot_at_epoch(epoch) - 1) or the genesis block root in the case of underflow.
+        Args:
+            epoch: provide all proposers for the given epoch value
+        """
+        return self._query_url(f"/eth/v1/validator/duties/proposer/{epoch}")
